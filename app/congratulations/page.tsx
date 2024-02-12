d3f14247ae00d1802af0156327b2127ab033785c
Uladzislau Naurasiuk <uladzislau.naurasiuk@netcracker.com>
2024-02-12 12:32:02 +0300

    fix for Anastasiya

--- app/congratulations/page.tsx
+++ app/congratulations/page.tsx
@@ -9,7 +9,7 @@ export default function Congratulations() {
       <p className="text-center mt-4">Ура! Ты справилась! Вот твой подарок!</p>
       {/* Feel free to remove this button if not needed */}
       <div className="flex items-center justify-center mt-4">
-        <Link href="/congratulations/gift" className="focus:outline-none text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:ring-pink-200 font-medium rounded-lg text-sm px-5 py-2.5 mb-2">Получить подарок</Link>
+          Твой подарок находится там, где настаивается мой)
       </div>
     </div>
   )
